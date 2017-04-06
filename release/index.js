0fbe5412bfd202f9e3e9ed10f277d0ba41f5c426
snoop088 <snoop088@gmail.com>
2017-04-06 15:26:59 +0300

    build test

---
+++ release/index.js
@@ -0,0 +1,8 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+__export(require('./datatable.module'));
+__export(require('./types'));
+__export(require('./components'));
+//# sourceMappingURL=index.js.map
